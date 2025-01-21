d06e337840238f809ae69180f7bdcb9baab98c96
Ruslan Makarov <ruslan.makarov@gmail.com>
2025-01-21 08:12:20 +0300

    Initial commit

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+declare module "vite-plugin-i18n-json-md" {
+  import type { Plugin } from "vite";
+
+  /**
+   * Configuration options for the I18N-JSON-Markdown plugin
+   * 
+   * @property {string} sourceDir - The directory where the source files are located.
+   * @property {string} outputDir - The directory where the output files will be generated.
+   * @property {string[]} locales - An array of locale strings, e.g. ["en", "es", "zh-CN"]. They are subdirectories of the sourceDir.
+   * @property {"dev" | "prod" | "both" | "off"} [mode] - The mode in which the plugin operates. Default is "both".
+   * @property {boolean} [minify] - Whether to minify the output files. Default is false.
+   * @property {"json" | "json5" | "js"} [outputFormat] - The format of the output files. Default is "json".
+   */
+  export interface I18nJsonMdPluginOptions {
+    sourceDir: string;
+    outputDir: string;
+    locales: string[];
+    mode?: "dev" | "prod" | "both" | "off";
+    minify?: boolean;
+    outputFormat?: "json" | "json5" | "js";
+  }
+
+  /**
+   * Creates a Vite plugin for processing I18N JSON Markdown files.
+   * @param {I18nJsonMdPluginOptions} options - The configuration options for the plugin.
+   * @returns {Plugin} - The Vite plugin instance.
+   */
+  export function i18nJsonMdPlugin(options: I18nJsonMdPluginOptions): Plugin;
+} 
---
+++ index.ts
@@ -0,0 +1,138 @@
+import fs from "node:fs";
+import path from "node:path";
+import { glob } from "glob";
+import JSON5 from "json5";
+import { traverseJsonNodes } from "vite-plugin-json-md";
+
+import type { Plugin, ResolvedConfig } from "vite";
+
+/**
+ * Configuration options for the I18N-JSON-Markdown plugin
+ */
+export interface I18nJsonMdPluginOptions {
+  sourceDir: string;
+  outputDir: string;
+  locales: string[];
+  mode?: "dev" | "prod" | "both" | "off";
+  minify?: boolean;
+  outputFormat?: "json" | "json5" | "js";
+}
+
+export function i18nJsonMdPlugin(options: I18nJsonMdPluginOptions): Plugin {
+  const {
+    sourceDir,
+    outputDir,
+    locales,
+    mode = "both",
+    minify = false,
+    outputFormat = "json",
+  } = options;
+
+  let config: undefined | ResolvedConfig;
+
+  return {
+    name: "vite-plugin-i18n-json-md",
+    configResolved(resolvedConfig) {
+      config = resolvedConfig;
+    },
+
+    buildStart() {
+      if (
+        mode !== "both" &&
+        ((mode === "dev" && !config?.env.DEV) ||
+          (mode === "prod" && !config?.env.PROD))
+      ) {
+        return;
+      }
+
+      locales.forEach((locale) => {
+        handleLocale(locale);
+      });
+    },
+  };
+
+  function handleLocale(locale: string) {
+    const localeDir = path.join(sourceDir, locale);
+    const inputFiles = glob.sync(
+      path.join(localeDir, "**/*.{json5,jsonc,json}"),
+      { nodir: true },
+    );
+
+    // Combined object to store all parsed data
+    const combinedParsedData: Record<string, any> = {};
+
+    for (const filePath of inputFiles) {
+      const fileContent = fs.readFileSync(filePath, "utf-8");
+      const parsedData = JSON5.parse(fileContent);
+
+      // Recursively process markdown in the current file's data
+      traverseJsonNodes(parsedData, localeDir);
+
+      // Directly merge the parsed data into the combined object
+      deepMerge(combinedParsedData, parsedData);
+    }
+
+    // Ensure output directory exists
+    fs.mkdirSync(path.dirname(outputDir), { recursive: true });
+
+    // Write processed file
+    const outputPath = path.join(outputDir, `${locale}.${outputFormat}`);
+    if (outputFormat === "json") {
+      fs.writeFileSync(
+        outputPath,
+        minify
+          ? JSON.stringify(combinedParsedData)
+          : JSON.stringify(combinedParsedData, null, 2),
+      );
+    } else if (outputFormat === "json5") {
+      fs.writeFileSync(
+        outputPath,
+        minify
+          ? JSON5.stringify(combinedParsedData)
+          : JSON5.stringify(combinedParsedData, null, 2),
+      );
+    } else if (outputFormat === "js") {
+      const exportObjectName = `${locale}Locale`;
+      fs.writeFileSync(
+        outputPath,
+        `export const ${exportObjectName} = ${
+          minify
+            ? customStringify(combinedParsedData)
+            : customStringify(combinedParsedData, 2)
+        };\n` + `export default ${exportObjectName};\n`,
+      );
+    }
+  }
+}
+
+// Deep merge utility function
+function deepMerge(target: Record<string, any>, source: Record<string, any>) {
+  for (const [key, value] of Object.entries(source)) {
+    if (value instanceof Object && !Array.isArray(value)) {
+      // If target doesn't have the key, create an empty object
+      if (!target[key]) {
+        target[key] = {};
+      }
+
+      // If target[key] is not an object, replace it with an empty object
+      if (!(target[key] instanceof Object)) {
+        target[key] = {};
+      }
+
+      // Recursively merge nested objects
+      deepMerge(target[key], value);
+    } else {
+      // For non-object values, simply assign
+      target[key] = value;
+    }
+  }
+  return target;
+}
+
+// remove quotes from keys
+function customStringify(obj: any, space?: number): string {
+  return JSON.stringify(obj, (key, value) => value, space).replaceAll(
+    /"([a-z_$][\w$]*)":/gi,
+    "$1:",
+  );
+}
